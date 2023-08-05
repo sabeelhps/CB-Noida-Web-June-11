aca7d5b3e777e9aec10e4977a9734a59f6b63602
Sabeel Khan <sabeelhps@gmail.com>
2023-08-05 19:13:46 +0530

    Introduction to JS

---
+++ Lecture-11/JSIntro/app.js
@@ -0,0 +1,96 @@
+// console.log('Hello from app file!');
+
+// // multiline string
+
+
+// const str = `This 
+// is some text`;
+
+// console.log(str);
+
+// Conditonals
+
+// if (conditon) {
+//     your code
+// }
+
+// if (conditon) {
+    
+// } else {
+    
+// }
+
+// if (condition) {
+    
+// } else if (conditon2) {
+    
+// } else if (conditon3) {
+    
+// } else {
+    
+// }
+
+// const inp = prompt("Enter your name");
+
+// console.log(inp);
+
+
+// const age = parseInt(prompt("Enter your age!"));
+
+// if (age < 18) {
+//     alert('You cannot enter or drink');
+// } else if (age>=18 && age < 25) {
+//     alert('You can enter but cannot drink!');
+// } else if (age >= 25) {
+//     alert('You can enter and drink');
+// } else {
+//     alert('Invalid Age');
+// }
+
+// console.log('Hello');
+// console.log('World');
+
+
+// const month = 1;
+
+// switch (month) {
+//     case 1:
+//         console.log('January');
+//         break;
+//     case 2:
+//         console.log('Feb');
+//         break;
+//     case 3:
+//         console.log('March');
+//         break;
+// }
+
+// const status = "AUTHORISATION_FAILED";
+
+// switch (status) {
+//     case "SUCCESS":
+//         console.log('Payment success');
+//         break;
+//     case "FAILED":
+//         console.log('Payment Failed');
+//         break;
+//     case "IN_PROGRESS":
+//         console.log('Payment in progress');
+//         break;
+//     default:
+//         console.log('Invalid Payment Status');
+// }
+
+const dishName = prompt("Enter your dish name");
+
+if (dishName.toLowerCase().includes('chicken') || dishName.toLowerCase().includes('egg')) {
+    console.log('Non Veg');
+} else {
+    console.log('Veg');
+}
+
+
+
+
+
+
