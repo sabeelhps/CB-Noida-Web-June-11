b08ba858da9730bd2808932a69540d795adfebfd
Sabeel Khan <sabeelhps@gmail.com>
2023-08-06 18:52:53 +0530

    Arrays, Objects, Loops

--- Lecture-12/WarmUp/app.js
+++ Lecture-12/WarmUp/app.js
@@ -2,16 +2,36 @@
 // 25.874, 26.874
 // 25.986, 25.987
 
-const num1 = 3427625.883;
-const num2 = 3427625.883;
+// const num1 = 3427625.883;
+// const num2 = 3427625.883;
 
-// check if integar parts are equal
+// // check if integar parts are equal
 
-const firstNum = num1 * 1000;
-const secondNum = num2 * 1000;
+// const firstNum = num1 * 1000;
+// const secondNum = num2 * 1000;
 
-if (parseInt(firstNum)-parseInt(secondNum)===0) {
-    console.log('Numbers are equal');
-} else {
-    console.log('Not equal');
+// if (parseInt(firstNum)-parseInt(secondNum)===0) {
+//     console.log('Numbers are equal');
+// } else {
+//     console.log('Not equal');
+// }
+
+// ------------------------------
+
+// const sentence = "This fox jumps over a white lazy dog";
+// console.log(sentence);
+// const words = sentence.split(' ');
+// words.reverse();
+// const reversedSentence = words.join(' ');
+// console.log(reversedSentence);
+
+let n = 5;
+
+for (let row = 1; row <= n; row++){
+    let str = "";
+    for (let col = 1; col <= row; col++){
+        str += "* ";
+    }
+    console.log(str);
 }
+
