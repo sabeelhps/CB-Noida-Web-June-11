ee7ad52c57e458c05e0b12c4c3f6509688a6287b
Sabeel Khan <sabeelhps@gmail.com>
2023-08-13 16:41:01 +0530

    surprise test 1

---
+++ surprise-test-1.js
@@ -0,0 +1,85 @@
+// --------------------------Find area corresponding to each radius
+
+const radii = [2, 3, 1, 4, 6, 7, 8];
+
+
+// ---------------------------reverse words--------
+
+
+const sentence = "The blue fox jumps over a white lazy dog";
+
+// Expected ans = "dog lazy white a over jumps fox blue The";
+
+
+
+// --------------------------counts digit------------
+
+const num = 34125;
+
+// Expected ans = 5
+
+
+
+
+// ------------------------Count vowels----------
+
+const str = "Hello World";
+
+// Expected ans = 3
+
+
+
+
+// -------------------------calculate square root
+
+const numsArr = [49, 64, 81, 16, 50, 30];
+
+// Expected ans = [7, 8, 9, 4]
+
+
+
+// ------------------------frequency of each letter-------
+
+const message = "Hello World";
+
+// Expected ans - print frequency of each letter
+
+
+
+// ------------------------Sum of number elements
+
+const list = [1, 2, 3, "3476", 6, "43874", "2178832"];
+
+// Expected ans = 12 
+
+
+
+// ------------------------------sum of all even digit
+
+const number = 376347;
+
+// Expected ans = 10
+
+
+// ---------------------Capitalize the first letter of each word
+
+const str1 = "The blue fox jumps over a white lazy dog";
+
+// Expected ans = "The Blue Fox Jumps Over A White Lazy Dog";
+
+
+
+// ----------------------Remove repeated words from sentence--------------
+
+
+const sentence1 = "I dont repeat mistakes i date mistakes mistakes date repeat";
+
+
+// -----------------flattening the array----------
+
+
+const myArr = [[1, 2], [3, 4, 5], ["Hello", true], [5, [true, false]], 99, 100];
+
+// Expected result [1,2,3,4,5,"Hello",true,5,[true,false], 99, 100];
+
+
